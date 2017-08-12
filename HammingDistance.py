6ed05a478b383db773709c46325b07ec293b62c1
Vaporized <qqyesqq1@126.com>
2017-08-12 18:28:15 +0800

    init

---
+++ HammingDistance.py
@@ -0,0 +1,26 @@
+from conda.config import bits
+
+
+class Solution(object):
+    def hammingDistance(self, x, y):
+        """
+        :type x: int
+        :type y: int
+        :rtype: int
+        """
+        str_x = '{:b}'.format(x)
+        str_y = '{:b}'.format(y)
+        max_len = max(str_x.__len__(), str_y.__len__())
+        str_x = ('{:0>' + str(max_len) + '}').format(str_x)
+        str_y = ('{:0>' + str(max_len) + '}').format(str_y)
+
+        count = 0
+        for i in range(str_x.__len__()):
+            if str_x[i] != str_y[i]:
+                count += 1
+
+        return count
+
+
+solution = Solution()
+solution.hammingDistance(1, 4)
