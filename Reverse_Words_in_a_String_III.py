4447f1c9b4ccc5bc1bcf73c4f5aa9c2bb4c50e28
QQYES <qqyesqq1@126.com>
2017-08-12 20:56:11 +0800

    Reverse Words in a String III

--- Reverse_Words_in_a_String_III.py
+++ Reverse_Words_in_a_String_III.py
@@ -4,15 +4,8 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        result = ''
-        s_list = s.split(' ')
-        for word in s_list:
-            re_word = ''
-            for char in list(word).__reversed__():
-                re_word = re_word + char
-            result = result + re_word + ' '
-        print(result[:-1])
+        return ' '.join(map(lambda x: x[::-1], s.split()))
 
 
 solution = Solution()
-solution.reverseWords("Let's take LeetCode contest")
+print(solution.reverseWords("Let's take LeetCode contest"))
