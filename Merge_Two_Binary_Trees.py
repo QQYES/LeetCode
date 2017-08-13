c61bdd2a569b4232ccfd59493c0408d7e946818b
Vaporized <qqyesqq1@126.com>
2017-08-13 11:39:03 +0800

    Merge Two Binary Trees

--- Merge_Two_Binary_Trees.py
+++ Merge_Two_Binary_Trees.py
@@ -21,7 +21,20 @@ class Solution(object):
                 tree_node = None
         else:
             tree_node = TreeNode(t1.val + t2.val)
-            tree_node.left = TreeNode(t1.left.val + t2.left.val)
-            tree_node.right = TreeNode(t1.right.val + t2.right.val)
+            if t1.left or t2.left:
+                if t1.left:
+                    tree_node.left = TreeNode(t1.left.val)
+                elif t2.left:
+                    tree_node.left = TreeNode(t2.left.val)
+                else:
+                    tree_node.left = TreeNode(t1.left.val + t2.left)
+
+            if t1.right or t2.right:
+                if t1.right:
+                    tree_node.right = TreeNode(t1.right.val)
+                elif t2.right:
+                    tree_node.right = TreeNode(t2.right.val)
+                else:
+                    tree_node.right = TreeNode(t1.right.val + t2.right.val)
 
         return tree_node
