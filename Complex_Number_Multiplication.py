c33dc89a3300ff9e0b75192a681193749c21b0da
Vaporized <qqyesqq1@126.com>
2017-08-13 15:31:55 +0800

    Array Partition I

---
+++ Complex_Number_Multiplication.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def complexNumberMultiply(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: str
+        """
+        com_a = a.split('+')
+        com_b = b.split('+')
+        result = complex(int(com_a[0]), int(com_a[1].replace('i', ''))) * complex(int(com_b[0]),
+                                                                                  int(com_b[1].replace('i', '')))
+        print result
+
+
+
+
+solution = Solution()
+print(solution.complexNumberMultiply("1+-1i", "1"))
