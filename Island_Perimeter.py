605a3b410b984e27019bbd3d360adbeed62d53d9
Vaporized <qqyesqq1@126.com>
2017-08-15 00:02:47 +0800

    Island Perimeter

--- Island_Perimeter.py
+++ Island_Perimeter.py
@@ -9,13 +9,21 @@ class Solution(object):
         for index_out, sublist in enumerate(grid):
             for index_in, elem in enumerate(sublist):
                 if elem:
-                    if elem[index_in - 1] == 0:
+                    if index_in == 0 or index_in == len(sublist) - 1:
                         count += 1
-                    if elem[index_in + 1] == 0:
-                        count += 1
+                    else:
+                        if sublist[index_in - 1] == 0:
+                            count += 1
+                        if sublist[index_in + 1] == 0:
+                            count += 1
 
-                    if index_out == 1 or index_out == len(grid):
+                    if index_out == 0 or index_out == len(grid) - 1:
                         count += 1
+                    else:
+                        if grid[index_out - 1][index_in] == 0:
+                            count += 1
+                        if grid[index_out + 1][index_in] == 0:
+                            count += 1
 
         return count
 
