5a0aa163f78a416effbe041587c266901b8804d3
Vaporized <qqyesqq1@126.com>
2017-08-14 18:27:22 +0800

    Island Perimeter

--- Island_Perimeter.py
+++ Island_Perimeter.py
@@ -4,11 +4,24 @@ class Solution(object):
         :type grid: List[List[int]]
         :rtype: int
         """
-        if isinstance(grid, list):
-            count = 0
-            for index_out, sublist in enumerate(grid):
-                for index_in, elem in enumerate(sublist):
-                    if elem:
-                        if elem[index_in - 1] == 0:
 
-                            if index_out == 1 or index_out == len(grid):
+        count = 0
+        for index_out, sublist in enumerate(grid):
+            for index_in, elem in enumerate(sublist):
+                if elem:
+                    if elem[index_in - 1] == 0:
+                        count += 1
+                    if elem[index_in + 1] == 0:
+                        count += 1
+
+                    if index_out == 1 or index_out == len(grid):
+                        count += 1
+
+        return count
+
+
+solution = Solution()
+print(solution.islandPerimeter([[0, 1, 0, 0],
+                                [1, 1, 1, 0],
+                                [0, 1, 0, 0],
+                                [1, 1, 0, 0]]))
