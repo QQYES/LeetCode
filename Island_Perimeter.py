32a9699d3a667b5c6952fd514ae62e325e57cda3
QQYES <qqyesqq1@126.com>
2017-08-14 17:25:06 +0800

    Island Perimeter

--- Island_Perimeter.py
+++ Island_Perimeter.py
@@ -4,3 +4,11 @@ class Solution(object):
         :type grid: List[List[int]]
         :rtype: int
         """
+        if isinstance(grid, list):
+            count = 0
+            for index_out, sublist in enumerate(grid):
+                for index_in, elem in enumerate(sublist):
+                    if elem:
+                        if elem[index_in - 1] == 0:
+
+                            if index_out == 1 or index_out == len(grid):
