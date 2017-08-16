1abdcc104d6f5d1bf30882f0d149860c2b13576e
Vaporized <qqyesqq1@126.com>
2017-08-16 22:24:45 +0800

    Queue Reconstruction by Height

--- Queue_Reconstruction_by_Height.py
+++ Queue_Reconstruction_by_Height.py
@@ -4,30 +4,6 @@ class Solution(object):
         :type people: List[List[int]]
         :rtype: List[List[int]]
         """
-        if isinstance(people, list):
-            stop = 0
-            while not stop:
-                for i, person in enumerate(people):
-                    inner_stop = 0
-                    if isinstance(person, list):
-                        count = 0
-                        for j, person_com in enumerate(people):
-                            if i != j:
-                                if count == person[1]:
-                                    if i < j:
-                                        break
-                                if person_com[0] >= person[0]:
-                                    if i < j:
-                                        people[j], people[i] = people[i], people[j]
-                                        i = j
-                                        inner_stop = 1
-                                    count += 1
-                    if i == len(people) - 1 and inner_stop == 0:
-                        stop = 1
-                    if inner_stop == 1:
-                        break
-
-            return people
 
 
 solution = Solution()
