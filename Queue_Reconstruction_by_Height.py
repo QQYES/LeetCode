293404f061d6330fb80fc5527b449ee2f3cb331a
Vaporized <qqyesqq1@126.com>
2017-08-16 22:24:22 +0800

    Island Perimeter

--- Queue_Reconstruction_by_Height.py
+++ Queue_Reconstruction_by_Height.py
@@ -10,17 +10,22 @@ class Solution(object):
                 for i, person in enumerate(people):
                     inner_stop = 0
                     if isinstance(person, list):
+                        count = 0
                         for j, person_com in enumerate(people):
-                            count = 0
-                            if count == person[1]:
-                                break
-                            if person_com[0] >= person[0]:
-                                people[j], people[i] = people[i], people[j]
-                                count += 1
-                                inner_stop = 1
+                            if i != j:
+                                if count == person[1]:
+                                    if i < j:
+                                        break
+                                if person_com[0] >= person[0]:
+                                    if i < j:
+                                        people[j], people[i] = people[i], people[j]
+                                        i = j
+                                        inner_stop = 1
+                                    count += 1
                     if i == len(people) - 1 and inner_stop == 0:
                         stop = 1
-                    break
+                    if inner_stop == 1:
+                        break
 
             return people
 
