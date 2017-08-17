a451caae77b615c1fe9ed6d74db366ab59404091
QQYES <qqyesqq1@126.com>
2017-08-17 16:18:11 +0800

    Queue Reconstruction by Height

--- Queue_Reconstruction_by_Height.py
+++ Queue_Reconstruction_by_Height.py
@@ -5,6 +5,11 @@ class Solution(object):
         :rtype: List[List[int]]
         """
 
+        res = []
+        for p in sorted(people, key=lambda x: x[0], reverse=True):
+            res.insert(p[1], p)
+        return res
+
 
 solution = Solution()
-print(solution.reconstructQueue([[7, 0], [4, 4], [7, 1], [5, 0], [6, 1], [5, 2]]))
+print(solution.reconstructQueue([[9, 0], [7, 0], [1, 9], [3, 0], [2, 7], [5, 3], [6, 0], [3, 4], [6, 2], [5, 2]]))
