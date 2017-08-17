9a93ca0f16834d196e3ed74940a856e5242e9214
QQYES <qqyesqq1@126.com>
2017-08-17 23:48:39 +0800

    Single Element in a Sorted Array

--- Single_Element_in_a_Sorted_Array.py
+++ Single_Element_in_a_Sorted_Array.py
@@ -4,7 +4,7 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        for i in range(0, len(nums) - 2, 2):
+        for i in range(0, len(nums) - 1, 2):
             if nums[i] != nums[i + 1]:
                 return nums[i]
         return nums[-1]
