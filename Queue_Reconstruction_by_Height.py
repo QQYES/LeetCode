703b8c8ac192f7e33e575945291d9a223c3359fb
Vaporized <qqyesqq1@126.com>
2017-08-17 12:54:18 +0800

    Queue Reconstruction by Height

--- Queue_Reconstruction_by_Height.py
+++ Queue_Reconstruction_by_Height.py
@@ -4,7 +4,10 @@ class Solution(object):
         :type people: List[List[int]]
         :rtype: List[List[int]]
         """
+        if isinstance(people, list):
+            tmp = sorted(people, key=lambda x: (-x[0], x[1]))
+            print(tmp)
 
 
 solution = Solution()
-print(solution.reconstructQueue([[7, 0], [4, 4], [7, 1], [5, 0], [6, 1], [5, 2]]))
+solution.reconstructQueue([[7, 0], [4, 4], [7, 1], [5, 0], [6, 1], [5, 2]])
