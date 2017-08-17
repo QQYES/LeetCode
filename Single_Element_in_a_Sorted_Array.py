c5f8a1da7922ce6a91f733283874241837359f41
QQYES <qqyesqq1@126.com>
2017-08-17 18:14:58 +0800

    Single Element in a Sorted Array

--- Single_Element_in_a_Sorted_Array.py
+++ Single_Element_in_a_Sorted_Array.py
@@ -7,6 +7,7 @@ class Solution(object):
         for i in range(0, len(nums) - 2, 2):
             if nums[i] != nums[i + 1]:
                 return nums[i]
+        return nums[-1]
 
 
 solution = Solution()
