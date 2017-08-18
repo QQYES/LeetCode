5d2f8de79a0f235d0fc3b211082df604438d99a5
QQYES <qqyesqq1@126.com>
2017-08-18 09:59:57 +0800

    Single Element in a Sorted Array

--- Find_All_Numbers_Disappeared_in_an_Array.py
+++ Find_All_Numbers_Disappeared_in_an_Array.py
@@ -4,3 +4,12 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
+        res = [i for i in range(min(nums), max(nums) + 1)]
+        for num in nums:
+            if num in res:
+                res.remove(num)
+        return res
+
+
+solution = Solution()
+print(solution.findDisappearedNumbers([4, 3, 2, 7, 8, 2, 3, 3, 3, 1]))
