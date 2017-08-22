02ea93eb161ce57dccc1fcd94b0a54d565ba2f83
QQYES <qqyesqq1@126.com>
2017-08-22 23:35:30 +0800

    Convert BST to Greater Tree

--- Convert_BST_to_Greater_Tree.py
+++ Convert_BST_to_Greater_Tree.py
@@ -5,9 +5,30 @@ class TreeNode(object):
         self.left = None
         self.right = None
 
+
 class Solution(object):
     def convertBST(self, root):
         """
         :type root: TreeNode
         :rtype: TreeNode
         """
+        if root:
+            if root.right:
+                root.val += root.right.val
+                return self.convertBST(root.right)
+
+            if root.left:
+                root.left.val += root.val
+                return self.convertBST(root.left)
+
+            return root
+        else:
+            return root
+
+
+solution = Solution()
+tree_node = TreeNode(5)
+tree_node.left = TreeNode(2)
+tree_node.right = TreeNode(13)
+res = solution.convertBST(tree_node)
+print(res.val, res.left.val, res.right.val)
