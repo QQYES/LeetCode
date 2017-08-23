2d38ee5c150724a5bf798a901abfb107a9ac07be
QQYES <qqyesqq1@126.com>
2017-08-23 16:41:45 +0800

    Convert BST to Greater Tree

--- Convert_BST_to_Greater_Tree.py
+++ Convert_BST_to_Greater_Tree.py
@@ -1,4 +1,7 @@
 # Definition for a binary tree node.
+from functools import reduce
+
+
 class TreeNode(object):
     def __init__(self, x):
         self.val = x
@@ -12,38 +15,34 @@ class Solution(object):
         :type root: TreeNode
         :rtype: TreeNode
         """
-        if root:
-            root.val = self.greater_add(root, root.val)
-            if root.right:
-                self.convertBST(root.right)
-            if root.left:
-                self.convertBST(root.left)
+        nodes = self.greater_add(root, [])
+        s = []
+        while root or s:
+            while root:
+
+                root.val += reduce(lambda x, y: x + y, [i for i in nodes if i > root.val])
+                s.append(root)
+                root = root.left
+
+            if s:
+                root = s.pop()
+                root = root.right
+
         return root
 
-    def greater_add(self, root, val):
+    def greater_add(self, root, nodes):
         """
         :param root: TreeNode
-        :param val: int
+        :param nodes: list
         :return: int
         """
         if root:
-            if val < root.val:
-                val += root.val
+            nodes.append(root.val)
             if root.right:
-                self.greater_add(root.right)
+                self.greater_add(root.right, nodes)
             if root.left:
-                self.greater_add(root.left)
-        return val
-
-    def visit(self, root, nodes):
-        """
-        :param root: TreeNode
-        :return: TreeNode
-        """
-        if root.left:
-            self.visit(root.left, nodes)
-        if root.right:
-            self.visit(root.left, nodes)
+                self.greater_add(root.left, nodes)
+        return nodes
 
 
 solution = Solution()
