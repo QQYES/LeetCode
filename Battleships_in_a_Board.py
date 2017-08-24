09bb65a40eed80c0c1e527a659aa55c1613fcf05
QQYES <qqyesqq1@126.com>
2017-08-24 16:37:36 +0800

    Battleships in a Board

--- Battleships_in_a_Board.py
+++ Battleships_in_a_Board.py
@@ -4,3 +4,26 @@ class Solution(object):
         :type board: List[List[str]]
         :rtype: int
         """
+        count = 0
+        if board is None:
+            return 0
+        for i in range(len(board)):
+            for j in range(len(board[i])):
+                if i != 0:
+                    if j != 0:
+                        if board[i][j] == '+':
+                            if board[i - 1][j] == '.' and board[i][j] == '.':
+                                count += 1
+                    else:
+                        if board[i][j] == '+':
+                            if board[i - 1][j] == '.':
+                                count += 1
+                else:
+                    if j != 0:
+                        if board[i][j] == '+':
+                            if board[i][j - 1] == '.':
+                                count += 1
+                    else:
+                        if board[i][j] == '+':
+                            count += 1
+        return count
