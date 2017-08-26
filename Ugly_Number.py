18cece132ec8258e919e0cd4456219db4fe172fb
Vaporized <qqyesqq1@126.com>
2017-08-26 08:19:31 +0800

    Ugly Number

--- Ugly_Number.py
+++ Ugly_Number.py
@@ -1,6 +1,25 @@
+from sympy import factor
+
+
 class Solution(object):
     def isUgly(self, num):
         """
         :type num: int
         :rtype: bool
         """
+        if num > 125 or num <= 0:
+            return False
+        elif num == 1:
+            return True
+        else:
+            for i in range(2, num + 1):
+                if num % i == 0:
+                    if i not in (2, 3, 5):
+                        return False
+                    else:
+                        num /= i
+        return True
+
+
+solution = Solution()
+print(solution.isUgly(8))
