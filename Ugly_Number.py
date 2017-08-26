d48c110406d33aa009ed200d3f75342fa0f62d93
Vaporized <qqyesqq1@126.com>
2017-08-26 23:34:17 +0800

    Ugly Number

--- Ugly_Number.py
+++ Ugly_Number.py
@@ -7,16 +7,21 @@ class Solution(object):
         :type num: int
         :rtype: bool
         """
-        while num > 1:
-            if num % 2 == 0:
-                num /= 2
-            elif num % 3 == 0:
-                num /= 3
-            elif num % 5 == 0:
-                num /= 5
-            else:
-                return False
-        return True
+        if num > 1:
+            while num > 1:
+                if num % 2 == 0:
+                    num /= 2
+                elif num % 3 == 0:
+                    num /= 3
+                elif num % 5 == 0:
+                    num /= 5
+                else:
+                    return False
+            return True
+        elif num == 1:
+            return True
+        else:
+            return False
 
 
 solution = Solution()
