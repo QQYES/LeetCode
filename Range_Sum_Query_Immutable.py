e7d8e4563bf7c1932364b7df301a861ee3d1959c
QQYES <qqyesqq1@126.com>
2017-08-28 17:16:51 +0800

    Range Sum Query - Immutable

---
+++ Range_Sum_Query_Immutable.py
@@ -0,0 +1,19 @@
+class NumArray(object):
+    def __init__(self, nums):
+        """
+        :type nums: List[int]
+        """
+
+    def sumRange(self, i, j):
+        """
+        :type i: int
+        :type j: int
+        :rtype: int
+        """
+
+
+# Your NumArray object will be instantiated and called as such:
+nums = []
+obj = NumArray(nums)
+i = j = 0
+param_1 = obj.sumRange(i, j)
