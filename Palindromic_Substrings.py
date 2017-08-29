8c1602defb74cb8d0105031cd36d29a01ec933de
QQYES <qqyesqq1@126.com>
2017-08-29 11:42:12 +0800

    Palindromic Substrings

--- Palindromic_Substrings.py
+++ Palindromic_Substrings.py
@@ -4,3 +4,17 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
+        n = len(s)
+        ans = 0
+        for center in range(2 * n - 1):
+            left = int(center / 2)
+            right = left + center % 2
+            while left >= 0 and right < n and s[left] == s[right]:
+                ans += 1
+                left -= 1
+                right += 1
+        return ans
+
+
+solution = Solution()
+print(solution.countSubstrings('abc'))
