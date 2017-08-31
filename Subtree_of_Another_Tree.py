15cf01d6064a4a87e19eed6dfcba42ebc7fc211a
QQYES <qqyesqq1@126.com>
2017-09-01 05:16:28 +0800

    572. Subtree of Another Tree

---
+++ Subtree_of_Another_Tree.py
@@ -0,0 +1,14 @@
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution(object):
+    def isSubtree(self, s, t):
+        """
+        :type s: TreeNode
+        :type t: TreeNode
+        :rtype: bool
+        """
