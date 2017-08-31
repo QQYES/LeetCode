5899e6db42b581e96684dff6590b6fca4e581d96
QQYES <qqyesqq1@126.com>
2017-09-01 04:35:53 +0800

    206. Reverse Linked List

--- Reverse_Linked_List.py
+++ Reverse_Linked_List.py
@@ -10,3 +10,20 @@ class Solution(object):
         :type head: ListNode
         :rtype: ListNode
         """
+        tmp = []
+        while head:
+            tmp.append(head.val)
+            head = head.next
+        if len(tmp) == 1:
+            head = ListNode(tmp[0])
+        elif len(tmp) > 0:
+            last = ListNode(tmp[0])
+            for i in range(1, len(tmp)):
+                head = ListNode(tmp[i])
+                head.next = last
+                last = head
+        return head
+
+
+solution = Solution()
+print(solution.reverseList(ListNode(1)))
