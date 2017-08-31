8090d2a0224a9377983c158f2f42ea5c487e6384
QQYES <qqyesqq1@126.com>
2017-08-31 15:43:04 +0800

    279. Perfect Squares

--- Perfect_Squares.py
+++ Perfect_Squares.py
@@ -1,6 +1,21 @@
+from math import floor
+
+
 class Solution(object):
     def numSquares(self, n):
         """
         :type n: int
         :rtype: int
         """
+        dp = []
+        while n > 0:
+            count = 0
+            i = int(floor(n ** 0.5))
+            while i > 0:
+                n -= i ** 2
+                count += 1
+                i = int(floor(n ** 0.5))
+
+
+solution = Solution()
+print(solution.numSquares(12))
