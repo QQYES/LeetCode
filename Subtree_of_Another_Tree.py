3dfeccf99dc03ba04c01b5fafc0bf6dda2c4e553
QQYES <qqyesqq1@126.com>
2017-09-01 23:18:02 +0800

    572. Subtree of Another Tree

--- Subtree_of_Another_Tree.py
+++ Subtree_of_Another_Tree.py
@@ -14,7 +14,7 @@ class Solution(object):
         :rtype: bool
         """
 
-        def visit(s_tree, t_tree, count):
+        def compare(s_tree, t_tree):
             """
             :param s_tree: TreeNode
             :param t_tree: TreeNode
@@ -22,22 +22,19 @@ class Solution(object):
             :return: None
             """
             while s_tree and t_tree:
-                if s_tree.val == t_tree.val:
-                    count.insert(0, 1)
-                    visit(s_tree.left, t_tree.left, count)
-                    visit(s_tree.right, t_tree.right, count)
-                else:
-                    count.insert(0, 0)
-                    visit(s_tree.left, t_tree, count)
-                    visit(s_tree.right, t_tree, count)
+                if s_tree is None and t_tree is None:
+                    return True
+                if s_tree is None or t_tree is None:
+                    return False
+                if s_tree.val != t_tree.val:
+                    return False
+                return compare(s_tree.left, t_tree.left) and compare(s_tree.right, t_tree.right)
 
-
-        count = []
-        visit(s, t, count)
-        if count[0] == 1:
-            return True
-        else:
+        if s is None:
             return False
+        if compare(s, t):
+            return True
+        return self.isSubtree(s.left, t) or self.isSubtree(s.right, t)
 
 
 solution = Solution()
@@ -47,6 +44,6 @@ s.left.left = TreeNode(1)
 s.left.right = TreeNode(2)
 s.right = TreeNode(5)
 t = TreeNode(4)
-t.right = TreeNode(2)
 t.left = TreeNode(1)
-solution.isSubtree(s, t)
+t.right = TreeNode(2)
+print(solution.isSubtree(s, t))
