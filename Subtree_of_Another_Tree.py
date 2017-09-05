8557a30380298fce9fe2043a1ef5b1e8e4bfe882
QQYES <qqyesqq1@126.com>
2017-09-05 11:00:14 +0800

    572. Subtree of Another Tree(切换为使用List)

--- Subtree_of_Another_Tree.py
+++ Subtree_of_Another_Tree.py
@@ -13,25 +13,24 @@ class Solution(object):
         :rtype: bool
         """
 
-        def compare(s_tree, t_tree):
+        def visit(root, l):
             """
-            :param s_tree: TreeNode
-            :param t_tree: TreeNode
-            :return: None
+            :param root: TreeNode
+            :param l: List[int]
             """
-            if s_tree is None and t_tree is None:
-                return True
-            if s_tree is None or t_tree is None:
-                return False
-            if s_tree.val != t_tree.val:
-                return False
-            return compare(s_tree.left, t_tree.left) and compare(s_tree.right, t_tree.right)
+            if root:
+                l.append(str(root.val))
+                visit(root.left, l)
+                visit(root.right, l)
 
-        if s is None:
-            return False
-        if compare(s, t):
+        s_list = []
+        t_list = []
+        visit(s, s_list)
+        visit(t, t_list)
+        if ''.join(s_list).__contains__(''.join(t_list)):
             return True
-        return self.isSubtree(s.left, t) or self.isSubtree(s.right, t)
+        else:
+            return False
 
 
 solution = Solution()
