0552ae987a52fa68a5bca02784e849a8cd0fbd54
QQYES <qqyesqq1@126.com>
2017-09-07 17:00:29 +0800

    List_to_Tree

--- List_to_Tree.py
+++ List_to_Tree.py
@@ -5,18 +5,28 @@ class TreeNode:
         self.right = None
 
 
-def init_tree(root, val):
-    """
-    :param val: int
-    :param root: TreeNode
-    :return: TreeNode
-    """
-    return TreeNode(val)
-
-
 def run(l):
-    head = TreeNode(None)
-    for item in l :
-        p=head
-        root=TreeNode(item)
+    queue = []
+    head = TreeNode(l[0])
+    queue.append(head)
+    for index in range(1, len(l), 2):
+        if l[index] != 'null':
+            left = TreeNode(l[index])
+        else:
+            left = None
+        queue.append(left)
+        if l[index + 1] != 'null':
+            right = TreeNode(l[index + 1])
+        else:
+            right = None
+        queue.append(right)
+        if len(queue) != 0:
+            p = queue.pop(0)
+            if p:
+                p.left = left
+                p.right = right
+    return head
 
+
+tree = run([10, 5, -3, 3, 2, 'null', 11, 3, -2, 'null', 1])
+print(tree)
