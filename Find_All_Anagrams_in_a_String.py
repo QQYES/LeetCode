81e4c4d51837d2b638b3aed8f00b5271d3b1ff10
Vaporized <qqyesqq1@126.com>
2017-09-11 23:21:28 +0800

    438. Find All Anagrams in a String(Time Limit Exceeded)

--- Find_All_Anagrams_in_a_String.py
+++ Find_All_Anagrams_in_a_String.py
@@ -10,7 +10,12 @@ class Solution(object):
         p_len = len(p)
         for i in range(len(s)):
             if len(s[i:i + p_len]) == p_len and len(p_set) == len(set(s[i:i + p_len])) and p_set == set(s[i:i + p_len]):
-                res.append(i)
+                flag = True
+                for item in p_set:
+                    if s[i:i + p_len].count(item) != p.count(item):
+                        flag = False
+                if flag:
+                    res.append(i)
         return res
 
 
