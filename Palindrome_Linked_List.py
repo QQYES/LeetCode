e61022d397c0fb7dd9a5b8bc8659b7992b5cad4a
QQYES <qqyesqq1@126.com>
2017-09-12 16:03:19 +0800

    234. Palindrome Linked List

--- Palindrome_Linked_List.py
+++ Palindrome_Linked_List.py
@@ -1,8 +1,12 @@
 # Definition for singly-linked list.
+from math import floor
+
+
 class ListNode(object):
     def __init__(self, x):
         self.val = x
         self.next = None
+
 
 class Solution(object):
     def isPalindrome(self, head):
@@ -10,3 +14,24 @@ class Solution(object):
         :type head: ListNode
         :rtype: bool
         """
+        mem = []
+        while head:
+            mem.append(head.val)
+            head = head.next
+        if len(mem) <= 1:
+            return True
+        if len(mem) > 1:
+            tmp1 = mem[:int((len(mem) / 2))]
+            tmp2 = [item for item in reversed(mem[int(len(mem) / 2):])]
+            # if mem[:(len(mem) / 2)] == reversed(mem[len(mem) / 2:]):
+            if tmp1 == tmp2:
+                return True
+
+        return False
+
+
+s = Solution()
+l = ListNode(1)
+l.next = ListNode(0)
+l.next.next = ListNode(1)
+print(s.isPalindrome(l))
