4b44db8d689cde2f2d36863356b802b53d2d338d
QQYES <qqyesqq1@126.com>
2017-09-21 11:33:59 +0800

    581. Shortest Unsorted Continuous Subarray

--- Shortest_Unsorted_Continuous_Subarray.py
+++ Shortest_Unsorted_Continuous_Subarray.py
@@ -4,3 +4,21 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        left = 0
+        right = 0
+        s_res = sorted(nums)
+        for i in range(len(nums)):
+            if s_res[i] != nums[i]:
+                left = i
+                break
+        for j in range(len(nums) - 1, 0, -1):
+            if s_res[i] != nums[i]:
+                right = j
+                break
+        return right - left
+        # print(nums)
+        # print(s_res)
+
+
+s = Solution()
+print(s.findUnsortedSubarray([2, 1]))
