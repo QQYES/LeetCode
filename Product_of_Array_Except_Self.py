32093aabf05e5b8a04f426f3910118701e18e69b
QQYES <qqyesqq1@126.com>
2017-09-30 11:09:24 +0800

    238. Product of Array Except Self(Accepted)

--- Product_of_Array_Except_Self.py
+++ Product_of_Array_Except_Self.py
@@ -1,6 +1,34 @@
+from functools import reduce
+
+
 class Solution(object):
     def productExceptSelf(self, nums):
         """
         :type nums: List[int]
         :rtype: List[int]
         """
+        zero_num = nums.count(0)
+        if zero_num > 1:
+            return [0] * len(nums)
+        if zero_num == 1:
+            sums = reduce(lambda x, y: x * y, list(filter(lambda x: x != 0, nums)))
+            res = []
+            for item in nums:
+                if item != 0:
+                    res.append(0)
+                else:
+                    res.append(sums)
+            return res
+        if zero_num == 0:
+            sums = reduce(lambda x, y: x * y, nums)
+            res = []
+            for item in nums:
+                if item != 0:
+                    res.append(sums / item)
+                else:
+                    res.append(sums)
+            return res
+
+
+s = Solution()
+print(s.productExceptSelf([1, 0]))
